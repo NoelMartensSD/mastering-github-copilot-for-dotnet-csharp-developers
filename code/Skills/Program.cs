4914fcb81155159257e153249ef5146cb9784f5c
NoelMartensSD <noelm@abe.midco.net>
2025-04-25 14:02:15 +0000

    Add subtraction and multiplication methods to Program

--- code/Skills/Program.cs
+++ code/Skills/Program.cs
@@ -6,6 +6,16 @@ void CalculateNumbers(int var1, int var2)
     Console.WriteLine($"The result of adding {var1} and {var2} is {result}");
 }
 
+void SubtractNumbers(int var1, int var2)
+{
+    int result = var1 - var2;
+    Console.WriteLine($"The result of subtracting {var1} from {var2} is {result}");
+}
+void MultiplyNumbers(int var1, int var2)
+{
+    int result = var1 * var2;
+    Console.WriteLine($"The result of multiplying {var1} and {var2} is {result}");
+}
 Console.WriteLine("Enter two numbers to add:");
 Console.Write("Number 1: ");
 var num1 = Convert.ToInt32(Console.ReadLine());
