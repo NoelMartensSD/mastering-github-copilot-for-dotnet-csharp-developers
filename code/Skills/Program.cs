85d484a1ad091c53406b55740151fcee75bf4c5a
NoelMartensSD <noelm@abe.midco.net>
2025-04-25 14:01:33 +0000

    Add initial implementation of Skills project with Program and project file

---
+++ code/Skills/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Transactions;
+
+void CalculateNumbers(int var1, int var2)
+{
+    int result = var1 + var2;
+    Console.WriteLine($"The result of adding {var1} and {var2} is {result}");
+}
+
+Console.WriteLine("Enter two numbers to add:");
+Console.Write("Number 1: ");
+var num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Number 2: ");
+var num2 = Convert.ToInt32(Console.ReadLine());
+CalculateNumbers(num1, num2);
